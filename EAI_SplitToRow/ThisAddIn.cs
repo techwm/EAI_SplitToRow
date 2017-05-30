12a6fa1b52e43d770c229629146d109e91a0c039
Eric Koay <Eric_Koay@jabil.com>
2017-05-31 07:08:46 +0800

    V1

--- EAI_SplitToRow/ThisAddIn.cs
+++ EAI_SplitToRow/ThisAddIn.cs
@@ -19,6 +19,67 @@ namespace EAI_SplitToRow
         {
         }
 
+        public void SplitToRow()
+        {
+            Excel.Application ExcelApp = Globals.ThisAddIn.Application;
+            Excel.Worksheet ActiveWSD = ExcelApp.ActiveSheet;
+            Excel.Range SelectedRng = ExcelApp.Selection;
+
+            //Select Impact Range, cannot multi area
+            Excel.Range ImpactRng;
+            RetryImpactRng:
+            try
+            {
+                ImpactRng = ExcelApp.InputBox("Please select range of your data", "Split To Row", Type: 8, Default: SelectedRng.Address[0,0]);
+            }
+            catch (Exception)
+            {
+                return;
+            }
+            if (ImpactRng.Areas.Count > 1)
+            {
+                System.Windows.Forms.MessageBox.Show("You can Only select 1 Area");
+                goto RetryImpactRng;
+            }
+
+            //Select Split Column
+            Excel.Range SplitRng;
+            RetrySplitRng:
+            try
+            {
+                SplitRng = ExcelApp.InputBox("Please select cells you want to split to Row", "Split To Row", Type: 8, Default: SelectedRng.Address[0, 0]);
+            }
+            catch (Exception)
+            {
+                return;
+            }
+            if (SplitRng.Areas.Count > 1)
+            {
+                System.Windows.Forms.MessageBox.Show("You can Only select 1 Area");
+                goto RetrySplitRng;
+            }
+
+            //Get Start Row & End Row
+            //sRow = splitColumn.Find("*", SearchOrder:= xlByRows, SearchDirection:= xlNext).Row - 1
+            //eRow = splitColumn.Find("*", SearchOrder:= xlByRows, SearchDirection:= xlPrevious).Row
+            int sRow = SplitRng.Find("*", SearchOrder: Excel.XlSearchOrder.xlByRows, SearchDirection: Excel.XlSearchDirection.xlNext).Row - 1;
+            int eRow = SplitRng.Find("*", SearchOrder: Excel.XlSearchOrder.xlByRows, SearchDirection: Excel.XlSearchDirection.xlPrevious).Row;
+            int nCol = SplitRng.Column;
+            SplitRng = ExcelApp.Range[ExcelApp.Cells[sRow, nCol], ExcelApp.Cells[eRow, nCol]];
+
+
+            //Get Splitter
+
+            //Define Split Point
+
+            //Loop Split Column
+            //Split Text in Split Point, r = count of split
+            //Copy & Insert Range
+
+
+            ActiveWSD.Range["A1"].Value = ImpactRng.Address;
+            ActiveWSD.Range["A2"].Value = SplitRng.Address;
+        }
         #region VSTO generated code
 
         /// <summary>
